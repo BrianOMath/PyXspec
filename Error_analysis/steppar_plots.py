c7147373abf14637e863b592c6004325ce9168f3
BrianOMath <100864707+BrianOMath@users.noreply.github.com>
2022-06-23 15:50:54 +0100

    Update steppar_plots.py

--- Error_analysis/steppar_plots.py
+++ Error_analysis/steppar_plots.py
@@ -15,7 +15,7 @@ Fit.nIterations = 50
 ## Steppar
 # O1 ##
 # nH
-#Fit.query = "no"
+#Fit.query = "yes"
 #Fit.steppar("2 0.0 10.0 20")
 
 ## Saving Plotsf
@@ -30,7 +30,7 @@ Fit.nIterations = 50
 
 
 ## Pho Index
-#Fit.query = "no"
+#Fit.query = "yes"
 #Fit.steppar("3 0.0 10.0 20")
 
 ## Saving Plots
@@ -45,7 +45,7 @@ Fit.nIterations = 50
 
 
 ## powerlaw norm
-#Fit.query = "no"
+#Fit.query = "yes"
 #Fit.steppar("4 0.0 1.0 20")
 
 ## Saving Plots
@@ -61,7 +61,7 @@ Fit.nIterations = 50
 
 
 ## Index1
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("5 0.0 10.0 20")
 
 ## Saving Plots
@@ -76,7 +76,7 @@ xspec.Plot.addCommand("index1/cps")
 
 
 ## gamma
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("13 0.0 10.0 20")
 
 ## Saving Plots
@@ -92,7 +92,7 @@ xspec.Plot.addCommand("gamma_o1/cps")
 
 
 ## logxi
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("14 0.0 4.7 20")
 
 ## Saving Plots
@@ -108,7 +108,7 @@ xspec.Plot.addCommand("logxi_o1/cps")
 
 
 ## logN
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("16 15.0 19.0 20")
 
 ## Saving Plots
@@ -125,7 +125,7 @@ xspec.Plot.addCommand("logN/cps")
 
 
 ## refl_frac
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("17 0.0 10.0 20 ")
 
 ## Saving Plots
@@ -142,7 +142,7 @@ xspec.Plot.addCommand("refl_frac_o1/cps")
 
 
 ## relxillD norm
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("18 0.0 1000.0 20")
 
 ## Saving Plots
@@ -159,7 +159,7 @@ xspec.Plot.addCommand("norm_relxillD_o1/cps")
 
 
 ## PSDsoft r_trc
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("20 4.0 15.0 40")
 
 ## Saving Plots
@@ -176,7 +176,7 @@ xspec.Plot.addCommand("r_trc_o1/cps")
 
 
 ## PSDsoft r_sh
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("21 3.0 100.0 40")
 
 ## Saving Plots
@@ -193,7 +193,7 @@ xspec.Plot.addCommand("r_sh_o1/cps")
 
 
 ## PSDsoft diskpar
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("23 0.001 0.1 20")
 
 ## Saving Plots
@@ -210,7 +210,7 @@ xspec.Plot.addCommand("disc_par_o1/cps")
 
 
 ## PSDsoft emiss
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("24 0.0 2.0 20")
 
 ## Saving Plots
@@ -226,7 +226,7 @@ xspec.Plot.addCommand("emiss_o1/cps")
 
 
 ## PSDsoft norm
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("25 0.0 1000 40")
 
 ## Saving Plots
@@ -243,7 +243,7 @@ xspec.Plot.addCommand("PSDsoft_norm_o1/cps")
 
 
 ## PSDhard norm
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("32 0.0 1000 40")
 
 ## Saving Plots
@@ -263,7 +263,7 @@ xspec.Plot.addCommand("PSDhard_norm_o1/cps")
 
 
 ## Pho Index
-#Fit.query = "no"
+#Fit.query = "yes"
 #Fit.steppar("108 0.0 10.0 20")
 
 ## Saving Plots
@@ -279,7 +279,7 @@ xspec.Plot.addCommand("PSDhard_norm_o1/cps")
 
 
 ## powerlaw norm
-#Fit.query = "no"
+#Fit.query = "yes"
 #Fit.steppar("109 0.0 1.0 20")
 
 ## Saving Plots
@@ -296,7 +296,7 @@ xspec.Plot.addCommand("PSDhard_norm_o1/cps")
 
 
 ## gamma
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("118 0.0 10.0 20")
 
 ## Saving Plots
@@ -312,7 +312,7 @@ xspec.Plot.addCommand("gamma_o2/cps")
 
 
 ## logxi
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("119 0.0 4.7 20")
 
 ## Saving Plots
@@ -329,7 +329,7 @@ xspec.Plot.addCommand("logxi_o2/cps")
 
 
 ## refl_frac
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("122 0.0 10.0 20 ")
 
 ## Saving Plots
@@ -346,7 +346,7 @@ xspec.Plot.addCommand("refl_frac_o2/cps")
 
 
 ## relxillD norm
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("123 0.0 1000.0 20")
 
 ## Saving Plots
@@ -363,7 +363,7 @@ xspec.Plot.addCommand("norm_relxillD_o2/cps")
 
 
 ## PSDsoft r_trc
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("125 4.0 15.0 40")
 
 ## Saving Plots
@@ -380,7 +380,7 @@ xspec.Plot.addCommand("r_trc_o2/cps")
 
 
 ## PSDsoft r_sh
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("126 3.0 100.0 40")
 
 ## Saving Plots
@@ -397,7 +397,7 @@ xspec.Plot.addCommand("r_sh_o2/cps")
 
 
 ## PSDsoft emiss
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("129 0.0 2.0 20")
 
 ## Saving Plots
@@ -413,7 +413,7 @@ xspec.Plot.addCommand("emiss_o2/cps")
 
 
 ## PSDsoft norm
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("130 0.0 1000 40")
 
 ## Saving Plots
@@ -430,7 +430,7 @@ xspec.Plot.addCommand("PSDsoft_norm_o2/cps")
 
 
 ## PSDhard norm
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("137 0.0 1000 40")
 
 ## Saving Plots
@@ -453,7 +453,7 @@ xspec.Plot.addCommand("PSDhard_norm_o2/cps")
 
 
 ## Pho Index
-#Fit.query = "no"
+#Fit.query = "yes"
 #Fit.steppar("213 0.0 10.0 20")
 
 ## Saving Plots
@@ -469,7 +469,7 @@ xspec.Plot.addCommand("PSDhard_norm_o2/cps")
 
 
 ## powerlaw norm
-#Fit.query = "no"
+#Fit.query = "yes"
 #Fit.steppar("214 0.0 1.0 20")
 
 ## Saving Plots
@@ -486,7 +486,7 @@ xspec.Plot.addCommand("PSDhard_norm_o2/cps")
 
 
 ## gamma
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("223 0.0 10.0 20")
 
 ## Saving Plots
@@ -502,7 +502,7 @@ xspec.Plot.addCommand("gamma_o3/cps")
 
 
 ## logxi
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("224 0.0 4.7 20")
 
 ## Saving Plots
@@ -519,7 +519,7 @@ xspec.Plot.addCommand("logxi_o3/cps")
 
 
 ## refl_frac
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("227 0.0 10.0 20 ")
 
 ## Saving Plots
@@ -536,7 +536,7 @@ xspec.Plot.addCommand("refl_frac_o3/cps")
 
 
 ## relxillD norm
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("228 0.0 1000.0 20")
 
 ## Saving Plots
@@ -553,7 +553,7 @@ xspec.Plot.addCommand("norm_relxillD_o3/cps")
 
 
 ## PSDsoft r_trc
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("230 4.0 15.0 40")
 
 ## Saving Plots
@@ -570,7 +570,7 @@ xspec.Plot.addCommand("r_trc_o3/cps")
 
 
 ## PSDsoft r_sh
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("231 3.0 100.0 40")
 
 ## Saving Plots
@@ -587,7 +587,7 @@ xspec.Plot.addCommand("r_sh_o3/cps")
 
 
 ## PSDsoft emiss
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("234 0.0 2.0 20")
 
 ## Saving Plots
@@ -603,7 +603,7 @@ xspec.Plot.addCommand("emiss_o3/cps")
 
 
 ## PSDsoft norm
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("235 0.0 1000 40")
 
 ## Saving Plots
@@ -620,7 +620,7 @@ xspec.Plot.addCommand("PSDsoft_norm_o3/cps")
 
 
 ## PSDhard norm
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("242 0.0 1000 40")
 
 ## Saving Plots
@@ -642,7 +642,7 @@ xspec.Plot.addCommand("PSDhard_norm_o3/cps")
 
 
 ## Pho Index
-#Fit.query = "no"
+#Fit.query = "yes"
 #it.steppar("318 0.0 10.0 20")
 
 ## Saving Plots
@@ -658,7 +658,7 @@ xspec.Plot.addCommand("PSDhard_norm_o3/cps")
 
 
 ## powerlaw norm
-#Fit.query = "no"
+#Fit.query = "yes"
 #Fit.steppar("319 0.0 1.0 20")
 
 ## Saving Plots
@@ -675,7 +675,7 @@ xspec.Plot.addCommand("PSDhard_norm_o3/cps")
 
 
 ## gamma
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("328 0.0 10.0 20")
 
 ## Saving Plots
@@ -691,7 +691,7 @@ xspec.Plot.addCommand("gamma_o4/cps")
 
 
 ## logxi
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("329 0.0 4.7 20")
 
 ## Saving Plots
@@ -707,7 +707,7 @@ xspec.Plot.addCommand("logxi_o4/cps")
 
 
 ## refl_frac
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("332 0.0 10.0 20 ")
 
 ## Saving Plots
@@ -724,7 +724,7 @@ xspec.Plot.addCommand("refl_frac_o4/cps")
 
 
 ## relxillD norm
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("333 0.0 1000.0 20")
 
 ## Saving Plots
@@ -741,7 +741,7 @@ xspec.Plot.addCommand("norm_relxillD_o4/cps")
 
 
 ## PSDsoft r_trc
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("335 4.0 15.0 40")
 
 ## Saving Plots
@@ -758,7 +758,7 @@ xspec.Plot.addCommand("r_trc_o4/cps")
 
 
 ## PSDsoft r_sh
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("336 3.0 100.0 40")
 
 ## Saving Plots
@@ -775,7 +775,7 @@ xspec.Plot.addCommand("r_sh_o4/cps")
 
 
 ## PSDsoft emiss
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("339 0.0 2.0 20")
 
 ## Saving Plots
@@ -791,7 +791,7 @@ xspec.Plot.addCommand("emiss_o4/cps")
 
 
 ## PSDsoft norm
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("340 0.0 1000 40")
 
 ## Saving Plots
@@ -808,7 +808,7 @@ xspec.Plot.addCommand("PSDsoft_norm_o4/cps")
 
 
 ## PSDhard norm
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("347 0.0 1000 40")
 
 ## Saving Plots
@@ -831,7 +831,7 @@ xspec.Plot.addCommand("PSDhard_norm_o4/cps")
 
 
 ## Pho Index
-#Fit.query = "no"
+#Fit.query = "yes"
 #Fit.steppar("423 0.0 10.0 20")
 
 ## Saving Plots
@@ -847,7 +847,7 @@ xspec.Plot.addCommand("PSDhard_norm_o4/cps")
 
 
 ## powerlaw norm
-#Fit.query = "no"
+#Fit.query = "yes"
 #Fit.steppar("424 0.0 1.0 20")
 
 ## Saving Plots
@@ -864,7 +864,7 @@ xspec.Plot.addCommand("PSDhard_norm_o4/cps")
 
 
 ## gamma
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("433 0.0 10.0 20")
 
 ## Saving Plots
@@ -880,7 +880,7 @@ xspec.Plot.addCommand("gamma_o5/cps")
 
 
 ## logxi
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("434 0.0 4.7 20")
 
 ## Saving Plots
@@ -897,7 +897,7 @@ xspec.Plot.addCommand("logxi_o5/cps")
 
 
 ## refl_frac
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("437 0.0 10.0 20 ")
 
 ## Saving Plots
@@ -914,7 +914,7 @@ xspec.Plot.addCommand("refl_frac_o5/cps")
 
 
 ## relxillD norm
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("438 0.0 1000.0 20")
 
 ## Saving Plots
@@ -931,7 +931,7 @@ xspec.Plot.addCommand("norm_relxillD_o5/cps")
 
 
 ## PSDsoft r_trc
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("440 4.0 15.0 40")
 
 ## Saving Plots
@@ -948,7 +948,7 @@ xspec.Plot.addCommand("r_trc_o5/cps")
 
 
 ## PSDsoft r_sh
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("441 3.0 100.0 40")
 
 ## Saving Plots
@@ -965,7 +965,7 @@ xspec.Plot.addCommand("r_sh_o5/cps")
 
 
 ## PSDsoft emiss
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("444 0.0 2.0 20")
 
 ## Saving Plots
@@ -981,7 +981,7 @@ xspec.Plot.addCommand("emiss_o5/cps")
 
 
 ## PSDsoft norm
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("445 0.0 1000 40")
 
 ## Saving Plots
@@ -998,7 +998,7 @@ xspec.Plot.addCommand("PSDsoft_norm_o5/cps")
 
 
 ## PSDhard norm
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("452 0.0 1000 40")
 
 ## Saving Plots
@@ -1022,7 +1022,7 @@ xspec.Plot.addCommand("PSDhard_norm_o5/cps")
 
 
 ## Pho Index
-#Fit.query = "no"
+#Fit.query = "yes"
 #Fit.steppar("528 0.0 10.0 20")
 
 ## Saving Plots
@@ -1038,7 +1038,7 @@ xspec.Plot.addCommand("PSDhard_norm_o5/cps")
 
 
 ## powerlaw norm
-#Fit.query = "no"
+#Fit.query = "yes"
 #Fit.steppar("529 0.0 1.0 20")
 
 ## Saving Plots
@@ -1055,7 +1055,7 @@ xspec.Plot.addCommand("PSDhard_norm_o5/cps")
 
 
 ## gamma
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("538 0.0 10.0 20")
 
 ## Saving Plots
@@ -1071,7 +1071,7 @@ xspec.Plot.addCommand("gamma_o6/cps")
 
 
 ## logxi
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("539 0.0 4.7 20")
 
 ## Saving Plots
@@ -1088,7 +1088,7 @@ xspec.Plot.addCommand("logxi_o6/cps")
 
 
 ## refl_frac
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("542 0.0 10.0 20 ")
 
 ## Saving Plots
@@ -1105,7 +1105,7 @@ xspec.Plot.addCommand("refl_frac_o6/cps")
 
 
 ## relxillD norm
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("543 0.0 1000.0 20")
 
 ## Saving Plots
@@ -1122,7 +1122,7 @@ xspec.Plot.addCommand("norm_relxillD_o6/cps")
 
 
 ## PSDsoft r_trc
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("545 4.0 15.0 40")
 
 ## Saving Plots
@@ -1139,7 +1139,7 @@ xspec.Plot.addCommand("r_trc_o6/cps")
 
 
 ## PSDsoft r_sh
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("546 3.0 100.0 40")
 
 ## Saving Plots
@@ -1156,7 +1156,7 @@ xspec.Plot.addCommand("r_sh_o6/cps")
 
 
 ## PSDsoft emiss
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("549 0.0 2.0 20")
 
 ## Saving Plots
@@ -1172,7 +1172,7 @@ xspec.Plot.addCommand("emiss_o6/cps")
 
 
 ## PSDsoft norm
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("550 0.0 1000 40")
 
 ## Saving Plots
@@ -1189,7 +1189,7 @@ xspec.Plot.addCommand("PSDsoft_norm_o6/cps")
 
 
 ## PSDhard norm
-Fit.query = "no"
+Fit.query = "yes"
 Fit.steppar("557 0.0 1000 40")
 
 ## Saving Plots
